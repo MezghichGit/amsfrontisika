514617d89d57a8b9edc66f4ef2b007c8bbdd7f81
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2022-04-22 16:41:26 +0100

    update provider

--- src/app/update-provider/update-provider.component.ts
+++ src/app/update-provider/update-provider.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
+import { ProviderService } from './../services/provider.service';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-update-provider',
   templateUrl: './update-provider.component.html',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateProviderComponent implements OnInit {
 
-  constructor() { }
+  public id: any;
+  public providerToUpdate: any;
+  public name: any;
+  public email: any;
+  public adress: any;
+  constructor(private service: ProviderService, private router: Router, private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.route.paramMap.subscribe(
+      params => {
+        this.id = params.get('id');
+      }
+    );
+    this.providerToUpdate = this.service.getProvider(this.id).subscribe(
+      response => {
+        //console.log(response);
+        this.name = response["name"];
+        this.email = response["email"];
+        this.adress = response["address"];
+      }
+    );
+    // this.initFormUpdateProvider(myform);
+  }
+
+  updateProvider() {
+    this.providerToUpdate = {
+      'name': this.name,
+      'email': this.email,
+      'address': this.adress,
+      'id': this.id
+    }
+    this.service.updateProvider(this.providerToUpdate).subscribe(
+      response => {
+        console.log(response);
+        this.router.navigate(['listProvider']);
+      }
+    );
+
   }
 
 }
